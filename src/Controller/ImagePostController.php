a7c086abefeaae31d98ad6faba69fc40a80d81a8
012kirby210 <012kirby210@gmail.com>
2024-11-05 20:27:47 +0100

    Dispatch du message opportun au gestionnaire.

--- src/Controller/ImagePostController.php
+++ src/Controller/ImagePostController.php
@@ -40,7 +40,6 @@ class ImagePostController extends AbstractController
                            ValidatorInterface $validator,
                            PhotoFileManager $photoManager,
                            EntityManagerInterface $entityManager,
-                           PhotoPonkaficator $ponkaficator,
                            MessageBusInterface $messageBus)
     {
         /** @var UploadedFile $imageFile */
@@ -63,21 +62,8 @@ class ImagePostController extends AbstractController
         $entityManager->persist($imagePost);
         $entityManager->flush();
 
-        $message = new AddPonkaToImage();
+        $message = new AddPonkaToImage($imagePost);
         $messageBus->dispatch($message);
-
-        /*
-         * Start Ponkafication!
-         */
-        $updatedContents = $ponkaficator->ponkafy(
-            $photoManager->read($imagePost->getFilename())
-        );
-        $photoManager->update($imagePost->getFilename(), $updatedContents);
-        $imagePost->markAsPonkaAdded();
-        $entityManager->flush();
-        /*
-         * You've been Ponkafied!
-         */
 
         return $this->toJson($imagePost, 201);
     }
