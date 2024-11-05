02d26c73c3f95d73c11d1c42a83b340e161e1a4e
012kirby210 <012kirby210@gmail.com>
2024-11-05 20:27:27 +0100

    Inclusion de l'image à modifier dans le message transmis.

--- src/Message/AddPonkaToImage.php
+++ src/Message/AddPonkaToImage.php
@@ -2,7 +2,22 @@
 
 namespace App\Message;
 
+use App\Entity\ImagePost;
+
 class AddPonkaToImage
 {
+    /**
+     * @var ImagePost
+     */
+    private $imagePost;
 
+    public function __construct(ImagePost $imagePost)
+    {
+        $this->imagePost = $imagePost;
+    }
+
+    public function getImagePost(): ImagePost
+    {
+        return $this->imagePost;
+    }
 }
