7a2b47e91720c6f76a79e293ccc91305c6204f3f
012kirby210 <012kirby210@gmail.com>
2024-11-05 20:27:02 +0100

    Déplacement de la logique de traitement de l'image dans le gestionnaire du message.

--- src/MessageHandler/AddPonkaToImageToHandler.php
+++ src/MessageHandler/AddPonkaToImageToHandler.php
@@ -3,12 +3,32 @@
 namespace App\MessageHandler;
 
 use App\Message\AddPonkaToImage;
+use App\Photo\PhotoFileManager;
+use App\Photo\PhotoPonkaficator;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
 class AddPonkaToImageToHandler implements MessageHandlerInterface
 {
+
+    private $ponkaficator;
+    private $photoManager;
+    private $entityManager;
+    public function __construct(PhotoPonkaficator $ponkaficator,
+                                PhotoFileManager $photoFileManager,
+                                EntityManagerInterface $entityManager){
+        $this->entityManager = $entityManager;
+        $this->photoManager = $photoFileManager;
+        $this->ponkaficator = $ponkaficator;
+    }
     public function __invoke(AddPonkaToImage $message)
     {
-        dump($message);
+        $imagePost = $message->getImagePost();
+        $updatedContents = $this->ponkaficator->ponkafy(
+            $this->photoManager->read($imagePost->getFilename())
+        );
+        $this->photoManager->update($imagePost->getFilename(), $updatedContents);
+        $imagePost->markAsPonkaAdded();
+        $this->entityManager->flush();
     }
 }
