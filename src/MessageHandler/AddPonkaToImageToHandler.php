f0a42c21b7608827274c3f58e4733b85bae82efc
012kirby210 <012kirby210@gmail.com>
2024-11-12 20:56:55 +0100

    Ajout de l'utilisation du concept par le gestionnaire du message et le contrôleur.

--- src/MessageHandler/AddPonkaToImageToHandler.php
+++ src/MessageHandler/AddPonkaToImageToHandler.php
@@ -2,34 +2,57 @@
 
 namespace App\MessageHandler;
 
+use App\Entity\ImagePost;
 use App\Message\AddPonkaToImage;
 use App\Photo\PhotoFileManager;
 use App\Photo\PhotoPonkaficator;
+use App\Repository\ImagePostRepository;
 use Doctrine\ORM\EntityManagerInterface;
+use Psr\Log\LoggerAwareInterface;
+use Psr\Log\LoggerAwareTrait;
 use Symfony\Component\Messenger\Handler\MessageHandlerInterface;
 
-class AddPonkaToImageToHandler implements MessageHandlerInterface
+class AddPonkaToImageToHandler implements MessageHandlerInterface, LoggerAwareInterface
 {
 
+    use LoggerAwareTrait;
     private $ponkaficator;
     private $photoManager;
     private $entityManager;
-    public function __construct(PhotoPonkaficator $ponkaficator,
-                                PhotoFileManager $photoFileManager,
-                                EntityManagerInterface $entityManager){
+    /**
+     * @var ImagePostRepository
+     */
+    private $imagePostRepository;
+    /**
+     * @var ImagePost
+     */
+    private $imagePost;
+
+    public function __construct(PhotoPonkaficator      $ponkaficator,
+                                PhotoFileManager       $photoFileManager,
+                                EntityManagerInterface $entityManager,
+                                ImagePostRepository    $imagePostRepository){
         $this->entityManager = $entityManager;
         $this->photoManager = $photoFileManager;
         $this->ponkaficator = $ponkaficator;
+        $this->imagePostRepository = $imagePostRepository;
     }
     public function __invoke(AddPonkaToImage $message)
     {
-        $imagePost = $message->getImagePost();
+        $imagePostId = $message->getImagePostId();
+        $imagePost = $this->imagePostRepository->find($imagePostId);
+
+        if (!$imagePost) {
+            // for testing purpose, the container could be unavailable
+            if ($this->logger){
+                $this->logger->alert(sprintf('Image post %d was missing', $imagePostId));
+            }
+        }
         $updatedContents = $this->ponkaficator->ponkafy(
             $this->photoManager->read($imagePost->getFilename())
         );
         $this->photoManager->update($imagePost->getFilename(), $updatedContents);
         $imagePost->markAsPonkaAdded();
-        $this->entityManager->persist($imagePost);
         $this->entityManager->flush();
     }
 }
