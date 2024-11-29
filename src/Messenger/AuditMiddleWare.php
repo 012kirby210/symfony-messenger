7355942dc86d39b33e21a99b08dc479b39f206ba
012kirby210 <012kirby210@gmail.com>
2024-11-29 18:15:06 +0100

    Création d'un contexte de requête dans AuditMiddleware.

--- src/Messenger/AuditMiddleWare.php
+++ src/Messenger/AuditMiddleWare.php
@@ -2,6 +2,7 @@
 
 namespace App\Messenger;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
 use Symfony\Component\Messenger\Middleware\StackInterface;
@@ -9,6 +10,11 @@ use Symfony\Component\Messenger\Middleware\StackInterface;
 class AuditMiddleWare implements MiddleWareInterface
 {
 
+    private LoggerInterface $logger;
+    public function __construct(LoggerInterface $messengerAuditLogger)
+    {
+        $this->logger = $messengerAuditLogger;
+    }
     public function handle(Envelope $envelope, StackInterface $stack): Envelope
     {
         if ( null === $envelope->last(UniqueIdStamp::class) ){
@@ -17,7 +23,12 @@ class AuditMiddleWare implements MiddleWareInterface
 
         /** @var UniqueIdStamp $stamp */
         $stamp = $envelope->last(UniqueIdStamp::class);
-        dump($stamp->getUniqueId());
+
+        $context = [
+            'id' => $stamp->getUniqueId(),
+            'class' => get_class($envelope->getMessage())
+        ];
+
         return $stack->next()->handle($envelope, $stack);
     }
 }
